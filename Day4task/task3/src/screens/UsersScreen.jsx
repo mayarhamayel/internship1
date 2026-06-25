2d03b5d15df88a070f03f99a1aad629f3a930490
mayarhamayel <mayargit4@gmail.com>
2026-06-25 10:26:26 +0300

    loading handel

--- Day4task/task3/src/screens/UsersScreen.jsx
+++ Day4task/task3/src/screens/UsersScreen.jsx
@@ -4,15 +4,20 @@ import { getUsers } from "../services/userService";
 function UsersScreen(){
 
     const[users,setUser]=useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
 
         const fetchUsersData = async () => {
           try {
+            setLoading(true);
             const data = await getUsers();
             setUser(data);
           } catch (error) {
             console.log("An error occurred while retrieving data.", error);
+          }finally{
+            setLoading(false);
+
           }
         };
 
@@ -22,18 +27,21 @@ function UsersScreen(){
 
     },[])
 
-    return (
-      <>
-        {users.map((user) => (
-          <div key={user.id}>
-                <h3>{user.name}</h3>
-                <p>{user.email}</p>
-            <p>{user.phone}</p>
-              
-          </div>
-        ))}
-      </>
-    );
+    if (loading){
+        return <h2>loading....</h2>;
+    }
+      return (
+        <>
+          {users.map((user) => (
+            <div key={user.id}>
+                  <h3>{user.name}</h3>
+                  <p>{user.email}</p>
+              <p>{user.phone}</p>
+                
+            </div>
+          ))}
+        </>
+      );
 }
 
 export default UsersScreen;
